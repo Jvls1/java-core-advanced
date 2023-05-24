fff89c5a634e1f66b08a23a808fd5666acaffafa
Joao Vitor <joaovlopes000@gmail.com>
2023-05-23 22:37:52 -0300

    - annotation processing impl

--- src/annotationprocessing/BuilderProcessor.java
+++ src/annotationprocessing/BuilderProcessor.java
@@ -5,8 +5,12 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import java.util.List;
 import java.util.Set;
+
+import static java.lang.annotation.ElementType.FIELD;
 
 @SupportedSourceVersion(SourceVersion.RELEASE_17)
 @SupportedAnnotationTypes("annotationprocessing.Builder")
@@ -14,6 +18,20 @@ public class BuilderProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        annotations.forEach(annotation -> {
+            roundEnv.getElementsAnnotatedWith(annotation)
+                    .forEach(this::generateBuilderFile);
+        });
         return false;
+    }
+
+    private void generateBuilderFile(Element element) {
+        String className = element.getSimpleName().toString();
+        String packageName = element.getEnclosingElement().toString();
+        String builderName = className + "Builder";
+        String builderFullName = packageName + "." + builderName;
+
+        List<? extends Element> fields = element.getEnclosedElements()
+                .stream().filter(e -> FIELD.equals(e.getKind())).toList();
     }
 }
