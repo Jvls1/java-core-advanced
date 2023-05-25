4d08cc7c41d10a88dff8d5ca0a977c05b8cb2fc5
Joao Vitor <joaovlopes000@gmail.com>
2023-05-24 21:15:03 -0300

    - annotation processing impl

--- src/annotationprocessing/BuilderProcessor.java
+++ src/annotationprocessing/BuilderProcessor.java
@@ -7,10 +7,12 @@ import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Set;
 
 import static java.lang.annotation.ElementType.FIELD;
+import static java.util.stream.Collectors.joining;
 
 @SupportedSourceVersion(SourceVersion.RELEASE_17)
 @SupportedAnnotationTypes("annotationprocessing.Builder")
@@ -33,5 +35,46 @@ public class BuilderProcessor extends AbstractProcessor {
 
         List<? extends Element> fields = element.getEnclosedElements()
                 .stream().filter(e -> FIELD.equals(e.getKind())).toList();
+
+        try (PrintWriter writer = new PrintWriter(
+                processingEnv.getFiler().createClassFile(builderName).openWriter())) {
+            writer.println("""
+                    package %s;
+                         
+                    public class %s {
+                    """
+                    .formatted(packageName, builderName)
+            );
+
+            fields.forEach(field ->
+                    writer.print("""
+                                private %s %s;
+                            """.formatted(field.asType().toString(), field.getSimpleName())
+                    )
+            );
+
+            writer.println();
+            fields.forEach(field ->
+                    writer.println("""
+                                public %s %s(%s value) {
+                                    %s = value;
+                                    return this;
+                                }
+                            """.formatted(builderName, field.getSimpleName(),
+                            field.asType().toString(), field.getSimpleName())
+                    )
+            );
+
+            writer.println("""
+                        public %s build() {
+                            return new %s(%s);
+                        }
+                    """.formatted(className, className,
+                    fields.stream().map(Element::getSimpleName).collect(joining(", ")))
+            );
+            writer.println("}");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
